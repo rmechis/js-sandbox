0b0182a828be9e08fc321df2e0ca0d4b4cedaf11
RafaMechis <144859564+rmechis@users.noreply.github.com>
2024-05-07 12:36:55 +0000

    modified

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('btn');
+    const button2 = document.getElementById('btn2');
     const mensagem = document.getElementById('mensagem');
 
     button.addEventListener('click', function() {
         mensagem.textContent = 'Oi, mundo!';
+    });
+
+    button2.addEventListener('click', function() {
+        mensagem.textContent = '';	
     });
 });
